7749563ccaf25a875f6a9a1c3740b84a0637877f
nelson199608 <130792780+nelson199608@users.noreply.github.com>
2023-04-20 02:59:08 -0400

    Update render.sh

--- render.sh
+++ render.sh
@@ -10,40 +10,21 @@ python3 -m simplebot -a "$ADDR" db -s "simplebot_downloader/delay" $DELAY
 python3 -m simplebot -a "$ADDR" db -s "simplebot_translator/filter_enabled" "no"
 python3 ./restore_keys.py
 
-# add the web_comress plugin
-python3 -c "import requests; r=requests.get('https://github.com/adbenitez/simplebot-scripts/raw/master/scripts/web_compress.py'); open('web_compress.py', 'wb').write(r.content)"
-python3 -m simplebot -a "$ADDR" plugin --add ./web_compress.py
 
 # add simple web server for service healthy 
 python3 -m simplebot -a "$ADDR" plugin --add ./simplebot_render.py
 
-# add the youtube plugin
-python3 -c "import requests; r=requests.get('https://github.com/adbenitez/simplebot-scripts/raw/master/scripts/youtube.py'); open('youtube.py', 'wb').write(r.content)"
-python3 -m simplebot -a "$ADDR" plugin --add ./youtube.py
-
-# add the web2pdf plugin
-sudo apt-get install wkhtmltopdf
-python3 -c "import requests; r=requests.get('https://github.com/nelson9608/simplebot-scripts/raw/master/scripts/web2pdf.py'); open('web2pdf.py', 'wb').write(r.content)"
-python3 -m simplebot -a "$ADDR" plugin --add ./web2pdf.py
 
 # add the web_search plugin
 python3 -c "import requests; r=requests.get('https://github.com/adbenitez/simplebot-scripts/raw/master/scripts/web_search.py'); open('web_search.py', 'wb').write(r.content)"
 python3 -m simplebot -a "$ADDR" plugin --add ./web_search.py
 
-# add the web_screenshot plugin
-sudo sysctl -w kernel.unprivileged_userns_clone 
-= 1
-python3 -c "import requests; r=requests.get('https://github.com/nelson9608/simplebot-scripts/raw/master/scripts/web_screenshot.py'); open('web_screenshot.py', 'wb').write(r.content)"
-python3 -m simplebot -a "$ADDR" plugin --add ./web_screenshot.py
 
 
 # add the encryption_error plugin to leverage key changes
 python3 -c "import requests; r=requests.get('https://github.com/adbenitez/simplebot-scripts/raw/master/scripts/encryption_error.py'); open('encryption_error.py', 'wb').write(r.content)"
 python3 -m simplebot -a "$ADDR" plugin --add ./encryption_error.py
 
-# add the misc search plugin
-python3 -c "import requests; r=requests.get('https://github.com/nelson9608/simplebot-scripts/raw/master/scripts/misc.py'); open('misc.py', 'wb').write(r.content)"
-python3 -m simplebot -a "$ADDR" plugin --add ./misc.py
 
 # add the wiki plugin
 python3 -c "import requests; r=requests.get('https://github.com/nelson9608/simplebot-scripts/raw/master/scripts/wikip.py'); open('wikip.py', 'wb').write(r.content)"
